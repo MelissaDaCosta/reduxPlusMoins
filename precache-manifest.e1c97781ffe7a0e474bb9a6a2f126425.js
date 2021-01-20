98445f60ca931f680fa45c1eb488a3559c37a1d4
MelissaDaCosta <mel-dacosta@hotmail.fr>
2021-01-20 21:21:18 +0100

    Updates

---
+++ precache-manifest.e1c97781ffe7a0e474bb9a6a2f126425.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41aa2a9cc342c665351f57073978d160",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "58116d3d77de4e77da3f",
+    "url": "/react-gh-pages/static/css/main.ca6b460a.chunk.css"
+  },
+  {
+    "revision": "3b47ca74b0840fe45b97",
+    "url": "/react-gh-pages/static/js/2.bf01f4af.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/react-gh-pages/static/js/2.bf01f4af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58116d3d77de4e77da3f",
+    "url": "/react-gh-pages/static/js/main.e09bf132.chunk.js"
+  },
+  {
+    "revision": "d3c37b8040821cc5fb7d",
+    "url": "/react-gh-pages/static/js/runtime-main.d7bfa57b.js"
+  }
+]);
